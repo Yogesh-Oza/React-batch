3be00f252d102447c65b50b14cffd0bde4ae2298
yogeshoza333335 <yogeshoza33333@gmail.com>
2024-07-22 13:27:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// jsx
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Button, Checkbox, TextField } from "@mui/material";
 
 function App() {
+  let name = "yogesh";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <h1>Hello from {name}</h1>
+      <p>This is p element</p>
+      <Button variant="contained" color="success">
+        Submit
+      </Button>
+      <TextField id="standard-basic" label="Name" variant="standard" />
+      <Checkbox label="required" defaultChecked color="success" />
+      <Footer />
     </div>
   );
 }
